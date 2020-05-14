4abc94d4e6cb48066e70e76e9b0f6a07906642f0
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-13 20:23:00 -0700

    email and address inputs added

--- client/src/components/InputUser.js
+++ client/src/components/InputUser.js
@@ -2,12 +2,14 @@ import React, { Fragment, useState } from 'react';
 
 const InputUser = () => {
 
-  const [name, setName] = useState("")
+  const [name, setName] = useState("Name");
+  const [email, setEmail] = useState("Email");
+  const [address, setAddress] = useState("Address");
 
   const onSubmitForm = async e => {
     e.preventDefault();
     try {
-      const body = { name };
+      const body = { name, email, address };
       const response = await fetch("http://localhost:5000/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -24,11 +26,21 @@ const InputUser = () => {
   <Fragment>
     <h1 className="text-center mt-5">User Database</h1>
     <form className="d-flex mt-5" onSubmit={onSubmitForm}>
-      <input 
+      <input
         type="text" 
         className="form-control" 
         value={name} 
         onChange={e => setName(e.target.value)}/>
+      <input
+        type="text" 
+        className="form-control" 
+        value={email} 
+        onChange={e => setEmail(e.target.value)}/>
+        <input
+        type="text" 
+        className="form-control" 
+        value={address} 
+        onChange={e => setAddress(e.target.value)}/>
       <button className="btn btn-success">Add User</button>
     </form>
   </Fragment>
